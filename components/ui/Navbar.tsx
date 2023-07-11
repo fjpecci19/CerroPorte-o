b8b4a8ce72ae1b58b7af58c3507b178ba88a1df8
[Franco <[fapeccisan@gmail.com]>
2023-07-11 19:09:52 -0400

    first commit

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <header className='w-full fixed top-0 left-0'>
+            <div className='md:flex h-28 justify-between bg-red-800 py-4 md:px-10 px-7'>
+                <div className='text-2xl cursor-pointer flex items-center text-white font-[Arial]'>
+                    <Link href={"/"}>
+                        <Image src='https://cdn.worldvectorlogo.com/logos/cerro-porteno.svg' alt='logo' width={90} height={90} className="h-full relative" />
+                    </Link>
+                </div>
+                <div className='text-2xl cursor-pointer flex items-center text-white font-[Arial] hover:text-blue-500'>
+                    <Link href={"/Arqueros"}>Arqueros</Link>
+                </div>
+                <div className='text-2xl cursor-pointer flex items-center text-white font-[Arial] hover:text-blue-500'>
+                    <Link href={"Defensores"}>Defensores</Link>
+                </div>
+                <div className='text-2xl cursor-pointer flex items-center text-white font-[Arial] hover:text-blue-500'>
+                    <Link href={"Mediocampistas"}>Mediocampistas</Link>
+                </div>
+                <div className='text-2xl cursor-pointer flex items-center text-white font-[Arial] hover:text-blue-500'>
+                    <Link href={"Delanteros"}>Delanteros</Link>
+                </div>
+                <div className='text-2xl cursor-pointer flex items-center text-white font-[Arial] hover:text-blue-500'>
+                    <Link href={"DT"}>Director Técnico</Link>
+                </div>
+            </div>
+        </header>
+    )
+}
